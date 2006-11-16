b46844429d5676c18d18e6e4a8ec547feab64e49
Douglas Hunt <dhunt@ucar.edu>
2006-11-16 16:36:44 +0000

    Import of DHUNT/PDL-NetCDF-0.92 from CPAN.
    gitpan-cpan-distribution: PDL-NetCDF
    gitpan-cpan-version:      0.92
    gitpan-cpan-path:         DHUNT/PDL-NetCDF-0.92.tar.gz
    gitpan-cpan-author:       DHUNT
    gitpan-cpan-maturity:     released

--- test.pl
+++ test.pl
@@ -1,27 +1,7 @@
-# Before `make install' is performed this script should be runnable with
-
-# `make test'. After `make install' it should work as `perl test.pl'
-
-######################### We start with some black magic to print on failure.
-
-# Change 1..1 below to 1..last_test_to_print .
-# (It may become useful if the test is moved to ./t subdirectory.)
-
-BEGIN { $| = 1; print "1..36\n"; }
-END {print "not ok 1\n" unless $loaded;}
-use PDL;
-use PDL::NetCDF;
-use PDL::Char;
-$loaded = 1;
-print "ok 1\n";
-
-######################### End of black magic.
-
-my $nok = 2;
-
-# Insert your test code below (better if it prints "ok 13"
-# (correspondingly "not ok 13") depending on the success of chunk 13
-# of the test code):
+use Test::More tests => 37;
+BEGIN { use_ok('PDL') };
+BEGIN { use_ok('PDL::NetCDF') };
+BEGIN { use_ok('PDL::Char') };
 
 #
 ## Test object-oriented interface
@@ -36,39 +16,32 @@ $out1 = $obj->get ('var1');
 my $str = "Station1  Station2  Station3  ";
 $obj->puttext('textvar', ['n_station', 'n_string'], [3,10], $str);
 my $outstr = $obj->gettext('textvar');
-$ok = ($str eq $outstr);
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ok($str eq $outstr, "puttext 1");
 
-$ok = ($in1 == $out1)->sum == $in1->nelem;
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ok ( ($in1 == $out1)->sum == $in1->nelem, "puttext 2");
 
 $in3 = pdl 1;
 $obj->put ('zeroDim', [], $in3);
 $out3 = $obj->get ('zeroDim');
-$ok = ($in3 == $out3);
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ok($in3 == $out3, "zeroDim");
 
 $dims  = pdl $in1->dims;
 $dims1 = pdl $out1->dims;
-$ok = ($dims == $dims1)->sum == $dims->nelem;
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ok( ($dims == $dims1)->sum == $dims->nelem, "puttext 3");
 
 $in2 = pdl [[1,2,3,4], [5,6,7,8]]; # dim1 is already 3, not 4
 eval { $obj->put ('var2', ['dim1', 'dim2'], $in2); }; # Dimension error
-$ok = ($@ =~ /Attempt to redefine length of dimension/);
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ok ($@ =~ /Attempt to redefine length of dimension/, "Dimension redefinition error");
 
 $pdlchar = PDL::Char->new ([['abc', 'def', 'hij'],['aaa', 'bbb', 'ccc']]);
 $obj->put ('varchar', ['dimc1', 'dimc2', 'dimc3'], $pdlchar);
 $charout = $obj->get('varchar');
-$ok = sum($pdlchar - $charout) == 0;
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ok(sum($pdlchar - $charout) == 0, "PDL::Char 1");
 
 $pdlchar1 = PDL::Char->new ("abcdefghiklm");
 $obj->put ('varchar1', ['dimc4'], $pdlchar1);
 $charout = $obj->get('varchar1');
-$ok = sum($pdlchar1 - $charout) == 0;
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ok(sum($pdlchar1 - $charout) == 0, "PDL::Char 2");
 
 # Test compressed put and get
 my $tol = 1e-6;
@@ -76,84 +49,71 @@ my $pi = 4*atan2(1,1);
 my $pdlc = PDL->new ($pi, $pi/2, $pi/4, $pi/8);
 $obj->put ('cpi', ['dimnew'], $pdlc, {COMPRESS => 1});
 my $pdlout = $obj->get('cpi');
-$ok = sum($pdlc - $pdlout) < $tol;
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" );
+ok(sum($pdlc - $pdlout) < $tol, "Compressed put/get 1");
 
 # try fetching compressed PDL
 my $pdlcomp = $obj->get('cpi', {NOCOMPRESS => 1});
 my $correct = pdl (2147483646, -306783379, -1533916891, -2147483648);
-$ok = sum($pdlcomp - $correct) < $tol;
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" );
+ok(sum($pdlcomp - $correct) < $tol, "Compressed put/get 2");
 
-$ok = !$obj->close;
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ok(!$obj->close, "Compressed put/get 3");
 
 # try a fast open
 my $nc1 = PDL::NetCDF->new('foo.nc', {TEMPLATE => $obj});
 my $varnames = $nc1->getvariablenames;
-$ok = grep(/^var1$/,@$varnames) + grep(/^textvar$/,@$varnames);
-print( ($ok==2 ? "ok ": "not ok "), $nok++, "\n" );
-$ok = !$nc1->close;
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" );
+ok(grep(/^var1$/,@$varnames) + grep(/^textvar$/,@$varnames), "Fast open 1");
+ok(!$nc1->close, "Fast open 2");
 
 # Try rewriting an existing file
 $obj1 = PDL::NetCDF->new ('>foo.nc', {PERL_SCALAR => 1, PDL_BAD => 1});
 my $varnames = $obj1->getvariablenames;
-$ok = grep(/^var1$/,@$varnames) + grep(/^textvar$/,@$varnames);
-print( ($ok==2 ? "ok ": "not ok "), $nok++, "\n" ); 
+ok(grep(/^var1$/,@$varnames) + grep(/^textvar$/,@$varnames), "Re-writing 1");
 
 my $dimnames = $obj1->getdimensionnames;
-$ok = grep(/^dim1$/,@$dimnames) + grep(/^dim2$/,@$dimnames) + grep(/^n_string$/,@$dimnames) + grep(/^n_station$/,@$dimnames);
-print( ($ok==4 ? "ok ": "not ok "), $nok++, "\n" ); 
+ok (grep(/^dim1$/,@$dimnames) + grep(/^dim2$/,@$dimnames) + grep(/^n_string$/,@$dimnames) + grep(/^n_station$/,@$dimnames), "Re-writing 2");
 
 $pdl = pdl [[1,2,3], [4,5,6]];
 $obj1->put ('var1', ['dim1', 'dim2'], $pdl);
 $pdl1 = $obj1->get ('var1');
 
-$ok = ($pdl1 == $pdl)->sum == $pdl->nelem;
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ok(($pdl1 == $pdl)->sum == $pdl->nelem, "2D put/get 1");
 
 $dims  = pdl $pdl->dims;
 $dims1 = pdl $pdl1->dims;
-$ok = ($dims == $dims1)->sum == $dims->nelem;
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ok(($dims == $dims1)->sum == $dims->nelem, "2D put/get 2");
 
 $attin = pdl [1,2,3];
-$rc = $obj1->putatt ($attin, 'double_attribute', 'var1');
-print( ($rc ? "not ok ": "ok "), $nok++, "\n" ); 
+ok(!$obj1->putatt ($attin, 'double_attribute', 'var1'), "Putatt 1");
 
 $attin2 = long [4,5];
-$rc = $obj1->putatt ($attin2, 'long_attribute', 'var1');
-print( ($rc ? "not ok ": "ok "), $nok++, "\n" ); 
+ok(!$obj1->putatt ($attin2, 'long_attribute', 'var1'), "Putatt 2");
 
 $attin3 = long [4];
-$rc = $obj1->putatt ($attin3, 'long_attribute1', 'var1');
-print( ($rc ? "not ok ": "ok "), $nok++, "\n" ); 
+ok (!$obj1->putatt ($attin3, 'long_attribute1', 'var1'), "Putatt 3");
 
 $attout = $obj1->getatt ('long_attribute1', 'var1');
-$ok = (!ref($attout) and $attout == 4);
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ok ( (!ref($attout) and $attout == 4), "Getatt 1");
 
 $attout = $obj1->getatt ('double_attribute', 'var1');
-$ok = ($attin == $attout)->sum == $attin->nelem;
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ok ( ($attin == $attout)->sum == $attin->nelem, "Getatt 2");
 
-$rc = $obj1->putatt ('Text Attribute', 'text_attribute');
-print( ($rc ? "not ok ": "ok "), $nok++, "\n" ); 
+ok (!$obj1->putatt ('Text Attribute', 'text_attribute'), "Putatt text");
 
 $attout = $obj1->getatt ('text_attribute');
-print( ($attout eq 'Text Attribute' ? "ok ": "not ok "), $nok++, "\n" ); 
+
 
 # test PDL_BAD option
 
-$pdl = pdl [[1,2,3], [4,5,-999]];
-$obj1->put ('var2', ['dim1', 'dim2'], $pdl);
-$attin = double([-999]);
-$rc = $obj1->putatt ($attin, '_FillValue', 'var2');
-print( ($rc ? "not ok ": "ok "), $nok++, "\n" ); 
-$pdl1 = $obj1->get ('var2');
-$ok = ($pdl1->nbad == 1);
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ SKIP: {
+   skip "Bad values not enabled", 2 unless ($PDL::Bad::Status == 1);
+
+   $pdl = pdl [[1,2,3], [4,5,-999]];
+   $obj1->put ('var2', ['dim1', 'dim2'], $pdl);
+   $attin = double([-999]);
+   ok (!$obj1->putatt ($attin, '_FillValue', 'var2'), "Badvals 1");
+   $pdl1 = $obj1->get ('var2');
+   ok ($pdl1->nbad == 1, "Badvals 2");
+ };
 
 # Put Slices
 #
@@ -162,48 +122,37 @@ print( ($ok ? "ok ": "not ok "), $nok++, "\n" );
 $out2 = $obj1->putslice('var2', ['dim1','dim2','dim3'],[2,3,2],[0,0,0],[2,3,1],$pdl1);
 
 $pdl2 = pdl [[7,8,9], [10,11,12]];
-$out2 = $obj1->putslice('var2',[] ,[] ,[0,0,1],[2,3,1],$pdl2);
-print( ($out2 ? "not ok ": "ok "), $nok++, "\n" ); 
+ok (! $obj1->putslice('var2',[] ,[] ,[0,0,1],[2,3,1],$pdl2), "Putslice 1");
 
 $pdlchar = PDL::Char->new (['a  ','def','ghi']);
-$out2 = $obj1->putslice('tvar', ['recNum','strlen'],[PDL::NetCDF::NC_UNLIMITED(),10],[0,0],[3,3],$pdlchar);
-print( ($out2 ? "not ok ": "ok "), $nok++, "\n" ); 
+ok ( ! $obj1->putslice('tvar', ['recNum','strlen'],[PDL::NetCDF::NC_UNLIMITED(),10],[0,0],[3,3],$pdlchar), "Putslice PDL::Char 1");
 
 $pdlchar = PDL::Char->new (['zzzz']);
-$out2 = $obj1->putslice('tvar',[],[],[5,0],[1,4],$pdlchar);
-print( ($out2 ? "not ok ": "ok "), $nok++, "\n" ); 
+ok (! $obj1->putslice('tvar',[],[],[5,0],[1,4],$pdlchar), "Putslice PDL::Char 2");
 
 $svar = short(27);
-$out2 = $obj1->putslice('svar', ['recNum'],[PDL::NetCDF::NC_UNLIMITED()],[0],[1],$svar);
-print( ($out2 ? "not ok ": "ok "), $nok++, "\n" ); 
+ok (! $obj1->putslice('svar', ['recNum'],[PDL::NetCDF::NC_UNLIMITED()],[0],[1],$svar), 
+    "Putslice short 1");
 
 $svar = short(13);
-$out2 = $obj1->putslice('svar', [],[],[8],[1],$svar);
-print( ($out2 ? "not ok ": "ok "), $nok++, "\n" ); 
-
+ok (! $obj1->putslice('svar', [],[],[8],[1],$svar), "Putslice short 2");
 
 # Get slices
 $out2 = $obj1->get ('var1', [1,1], [1,1]);
-$ok = ($out2 == 5);
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ok (($out2 == 5), "Get slice 1");
 
 $out2 = $obj1->get ('var1', [0,1], [2,1]);
-$ok = ($out2 == pdl[2,5])->sum == $out2->nelem;
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ok (($out2 == pdl[2,5])->sum == $out2->nelem, "Get slice 2");
 
 $out2 = $obj1->get ('var1', [0,1], [1,1]);
-$ok = ($out2 == 2);
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
-
-
+ok ( ($out2 == 2), "Get slice 3");
 
 # Test with a bogus file
 open (IN, ">bogus.nc");
 print IN "I'm not a netCDF file\n";
 close IN;
 eval { $obj2 = PDL::NetCDF->new ('bogus.nc'); };
-$ok = ($@ =~ /Not a netCDF file/);
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ok ($@ =~ /Not a netCDF file/, "Read bogus file");
 
 $obj = PDL::NetCDF->new ('foo1.nc');
 # test chars with unlimited dimension
@@ -216,11 +165,8 @@ for ('abc', 'defg', 'hijkl') {
 }
 $charout = $obj->get('char_unlim');
 $pdlchar = PDL::Char->new (['abc', 'defg', 'hijkl']);
-$ok = sum($pdlchar - $charout) == 0;
-print( ($ok ? "ok ": "not ok "), $nok++, "\n" ); 
+ok (sum($pdlchar - $charout) == 0, "chars with unlimited dimension");
 $obj->close;
-
-
 
 BEGIN {
   if(-e 'foo.nc'){
